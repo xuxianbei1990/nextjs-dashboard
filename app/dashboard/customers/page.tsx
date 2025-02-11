316f3289643a481f5742db7fb68bc67d174712e0
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2025-02-11 16:54:29 +0800

    learn

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+
+export default function CustomersPage() {
+    return <p>Customers Page</p>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import {lusitana} from "@/app/ui/fonts";
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -9,8 +11,13 @@ export default function Page() {
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <Image src="/hero-desktop.png" width={1000} height={760} alt="Hero Image"
+        className="hidden md:block" />
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div
+              className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+          />
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -18,10 +25,10 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
